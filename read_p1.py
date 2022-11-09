a6d631362a13c71a7659d50dd16f393d017a2943
Johan Spännare <johan.spannare@gmail.com>
2022-11-09 19:28:11 +0100

    .

--- read_p1.py
+++ read_p1.py
@@ -11,42 +11,78 @@ import sys
 import crcmod.predefined
 import re
 from tabulate import tabulate
+import mysql.connector
+import sqlite3
+from datetime import datetime
 
 # Change your serial port here:
 serialport = '/dev/ttyUSB0'
 
 # Enable debug if needed:
-debug = True
+debug = False
+
+# MySQL Host
+host = "127.0.0.1"
+conn = None
+curs = None
 
 # Add/update OBIS codes here:
+# obiscodes = {
+#     "0-0:1.0.0": "Timestamp",
+#     "0-0:96.3.10": "Switch electricity",
+#     "0-1:24.4.0": "Switch gas",
+#     "0-0:96.1.1": "Meter serial electricity",
+#     "0-1:96.1.1": "Meter serial gas",
+#     "0-0:96.14.0": "Current rate (1=day,2=night)",
+#     "1-0:1.8.1": "Rate 1 (day) - total consumption",
+#     "1-0:1.8.2": "Rate 2 (night) - total consumption",
+#     "1-0:2.8.1": "Rate 1 (day) - total production",
+#     "1-0:2.8.2": "Rate 2 (night) - total production",
+#     "1-0:21.7.0": "L1 consumption",
+#     "1-0:41.7.0": "L2 consumption",
+#     "1-0:61.7.0": "L3 consumption",
+#     "1-0:1.7.0": "All phases consumption",
+#     "1-0:22.7.0": "L1 production",
+#     "1-0:42.7.0": "L2 production",
+#     "1-0:62.7.0": "L3 production",
+#     "1-0:2.7.0": "All phases production",
+#     "1-0:32.7.0": "L1 voltage",
+#     "1-0:52.7.0": "L2 voltage",
+#     "1-0:72.7.0": "L3 voltage",
+#     "1-0:31.7.0": "L1 current",
+#     "1-0:51.7.0": "L2 current",
+#     "1-0:71.7.0": "L3 current",
+#     "0-1:24.2.3": "Gas consumption"
+#     }
+
 obiscodes = {
-    "0-0.1.0.0","Datum och tid",
-    "1-0:1.8.0","Mätarställning Aktiv Energi Uttag",
-    "1-0:2.8.0","Mätarställning Aktiv Energi Inmatning",
-    "1-0:3.8.0","Mätarställning Reaktiv Energi Uttag",
-    "1-0:4.8.0","Mätarställning Reaktiv Energi Inmatning",
-    "1-0:1.7.0","Aktiv Effekt Uttag	Momentan trefaseffekt",
-    "1-0:2.7.0","Aktiv Effekt Inmatning	Momentan trefaseffekt",
-    "1-0:3.7.0","Reaktiv Effekt Uttag	Momentan trefaseffekt",
-    "1-0:4.7.0","Reaktiv Effekt Inmatning	Momentan trefaseffekt",
-    "1-0:21.7.0","L1 Aktiv Effekt Uttag	Momentan effekt",
-    "1-0:22.7.0","L1 Aktiv Effekt Inmatning	Momentan effekt",
-    "1-0:41.7.0","L2 Aktiv Effekt Uttag	Momentan effekt",
-    "1-0:42.7.0","L2 Aktiv Effekt Inmatning	Momentan effekt",
-    "1-0:61.7.0","L3 Aktiv Effekt Uttag	Momentan effekt",
-    "1-0:62.7.0","L3 Aktiv Effekt Inmatning	Momentan effekt",
-    "1-0:23.7.0","L1 Reaktiv Effekt Uttag	Momentan effekt",
-    "1-0:24.7.0","L1 Reaktiv Effekt Inmatning	Momentan effekt",
-    "1-0:43.7.0","L2 Reaktiv Effekt Uttag	Momentan effekt",
-    "1-0:44.7.0","L2 Reaktiv Effekt Inmatning	Momentan effekt",
-    "1-0:63.7.0","L3 Reaktiv Effekt Uttag	Momentan effekt",
-    "1-0:64.7.0","L3 Reaktiv Effekt Inmatning	Momentan effekt",
-    "1-0:32.7.0","L1 Fasspänning	Momentant RMS-värde",
-    "1-0:52.7.0","L2 Fasspänning	Momentant RMS-värde",
-    "1-0:72.7.0","L3 Fasspänning	Momentant RMS-värde",
-    "1-0:31.7.0","L1 Fasström	Momentant RMS-värde",
-    "1-0:51.7.0","L2 Fasström	Momentant RMS-värde",
-    "1-0:71.7.0","L3 Fasström	Momentant RMS-värde"
+    "0-0.1.0.0":"Datum och tid",
+    "1-0:1.8.0":"Mätarställning Aktiv Energi Uttag",
+    "1-0:2.8.0":"Mätarställning Aktiv Energi Inmatning",
+    "1-0:3.8.0":"Mätarställning Reaktiv Energi Uttag",
+    "1-0:4.8.0":"Mätarställning Reaktiv Energi Inmatning",
+    "1-0:1.7.0":"Aktiv Effekt Uttag	Momentan trefaseffekt",
+    "1-0:2.7.0":"Aktiv Effekt Inmatning	Momentan trefaseffekt",
+    "1-0:3.7.0":"Reaktiv Effekt Uttag	Momentan trefaseffekt",
+    "1-0:4.7.0":"Reaktiv Effekt Inmatning	Momentan trefaseffekt",
+    "1-0:21.7.0":"L1 Aktiv Effekt Uttag	Momentan effekt",
+    "1-0:22.7.0":"L1 Aktiv Effekt Inmatning	Momentan effekt",
+    "1-0:41.7.0":"L2 Aktiv Effekt Uttag	Momentan effekt",
+    "1-0:42.7.0":"L2 Aktiv Effekt Inmatning	Momentan effekt",
+    "1-0:61.7.0":"L3 Aktiv Effekt Uttag	Momentan effekt",
+    "1-0:62.7.0":"L3 Aktiv Effekt Inmatning	Momentan effekt",
+    "1-0:23.7.0":"L1 Reaktiv Effekt Uttag	Momentan effekt",
+    "1-0:24.7.0":"L1 Reaktiv Effekt Inmatning	Momentan effekt",
+    "1-0:43.7.0":"L2 Reaktiv Effekt Uttag	Momentan effekt",
+    "1-0:44.7.0":"L2 Reaktiv Effekt Inmatning	Momentan effekt",
+    "1-0:63.7.0":"L3 Reaktiv Effekt Uttag	Momentan effekt",
+    "1-0:64.7.0":"L3 Reaktiv Effekt Inmatning	Momentan effekt",
+    "1-0:32.7.0":"L1 Fasspänning	Momentant RMS-värde",
+    "1-0:52.7.0":"L2 Fasspänning	Momentant RMS-värde",
+    "1-0:72.7.0":"L3 Fasspänning	Momentant RMS-värde",
+    "1-0:31.7.0":"L1 Fasström	Momentant RMS-värde",
+    "1-0:51.7.0":"L2 Fasström	Momentant RMS-värde",
+    "1-0:71.7.0":"L3 Fasström	Momentant RMS-värde"
     }
 
 def checkcrc(p1telegram):
@@ -109,6 +145,53 @@ def parsetelegramline(p1line):
     else:
         return ()
 
+# Upload data to server
+def uploadData(backlogData=False):
+    global c, upload, duration
+    try:
+        mydb = mysql.connector.connect(
+            host=host,
+            user="energy",
+            password="UKJkXjpBmgjeYhRZ",
+            database="Energy")
+        mycursor = mydb.cursor()
+        if backlogData == False:
+            sql = "INSERT INTO Log (Rate, Duration) VALUES (%s,%s)" % (str(c), duration)
+        else:
+            sql = "INSERT INTO Log (Logtime, Rate, Duration) VALUES %s"
+            try:
+                result = curs.execute("SELECT * FROM Log")
+                data = []
+                rows = 1
+                for row in result:
+                    data.append('("%s",%s)' % (row[0], row[1]))
+                    rows = rows + 1
+                val = ','.join(data)
+                val = val + ',(CURRENT_TIMESTAMP, %s, %s)' % (str(c),duration)
+                sql = sql % val
+            except (sqlite3.Error, sqlite3.Warning) as e:
+                print(" - Error %s [%s]" % (e, str(datetime.now())), flush=True)
+                return False
+        mycursor.execute(sql)
+        mydb.commit()
+
+        if backlogData == False:
+            if debug:
+                print(" + %s pulses uploaded." % str(c), flush=True)
+        else:
+            try:
+                curs.execute("DELETE FROM Log")
+                conn.commit()
+                if debug:
+                    print(" + %s records uploaded." % str(rows), flush=True)
+            except (sqlite3.Error, sqlite3.Warning) as e:
+                print(" - Error %s [%s]" % (e, str(datetime.now())), flush=True)
+        c = 0
+        duration = 0
+    except mysql.connector.Error as e:
+        print(" - Error %s [%s]" % (e, str(datetime.now())), flush=True)
+        print (" - Server Offline [%s]" % str(datetime.now()), flush=True)
+
 
 def main():
     ser = serial.Serial(serialport, 115200, xonxoff=1)
@@ -152,13 +235,16 @@ def main():
             ser.close()
             break
         except:
-            if debug:
-                print(traceback.format_exc())
+            # if debug:
+            #     print(traceback.format_exc())
             # print(traceback.format_exc())
             print ("Something went wrong...")
+            
+            # flush the buffer
+            ser.flush()
+            
             ser.close()
-        # flush the buffer
-        ser.flush()
+
 
 if __name__ == '__main__':
     main()
